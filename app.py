6d61d7c656965ef79d247675a33d706febb59917
Patrick-Lenihan <61756898+Patrick-Lenihan@users.noreply.github.com>
2021-03-13 20:08:31 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -1,21 +1,40 @@
 from flask import Flask, render_template
 from forms import CalenderInsertForm,HiddenFormForTodaysDate
 from database import get_db, close_db
-from functions_for_calender_page import insert_event_into_db, convert_from_string_time_to_int_format, returnDB
+from functions_for_calender_page import insert_event_into_db, convert_from_string_time_to_int_format, returnDB, convert_javascript_date_to_SQL_format
+from flask import session
+from flask_session import Session 
 app = Flask(__name__)
 app.config["SECRET_KEY"]="this-is-my-secret-key"
+app.config["SESSION_PERMANENT"] = False 
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 @app.teardown_appcontext
 def close_db_at_end_of_request(e=None):
     close_db(e)#automaticly close the db after each route
 
-
 @app.route("/calender", methods=["GET","POST"])
 def calender():
-    '''
-    this is a function that is the backend of the main calender webpage
-    '''
     hiddenForm = HiddenFormForTodaysDate()
+    output = ""
+    date_needed = 'True'
+    if hiddenForm.validate_on_submit():
+        if "client_date" not in session:
+            session["client_date"]= convert_javascript_date_to_SQL_format(hiddenForm.todaysDate.data)
+            #return the db
+            output = returnDB()
+        date_needed='False'
+        print(session['client_date'])
+        return render_template('calender.html',hiddenForm=hiddenForm, output=output, date_needed=date_needed)
+    return render_template('calender.html',hiddenForm=hiddenForm, output=output,date_needed=date_needed)
+
+
+@app.route("/insert", methods=["GET","POST"])
+def insert():
+    '''
+    this is a function that is the backend of the insersion webpage
+    '''
     form = CalenderInsertForm()
     output = returnDB()
     if form.validate_on_submit():
@@ -23,12 +42,10 @@ def calender():
         eventStart = convert_from_string_time_to_int_format(form.eventStart.data)
         eventEnd = convert_from_string_time_to_int_format(form.eventEnd.data)
         date = form.date.data
-        print(hiddenForm.todaysDate.data)
-        print(len(hiddenForm.todaysDate.data))
         insert_event_into_db(nameOfEvent,eventStart,eventEnd,date)
 
         #return the db
         output = returnDB()
-        return render_template('calender.html',hiddenForm=hiddenForm,form=form, output=output)
+        return render_template('insert.html',form=form)
     
-    return render_template('calender.html',hiddenForm=hiddenForm,form=form, output=output)
+    return render_template('insert.html',form=form)
--- functions_for_calender_page.py
+++ functions_for_calender_page.py
@@ -32,5 +32,4 @@ def convert_javascript_date_to_SQL_format(date):
     '''
     months = {"Jan":'01', "Feb":'02', "Mar":'03', "Apr":'04', "May":'05', "Jun":'06', "Jul":'07', "Aug":'08', "Sep":'09', "Oct":'10', "Nov":'11', "Dec":'12'}
     date = date.split(" ")
-    print(date)
     return date[3]+"-"+months[date[1]]+'-'+date[2]
